48cda976fb279441fdd7618e3b48d822a8939761
vipulverulkar <37355867+vipulverulkar@users.noreply.github.com>
2023-01-24 11:29:10 +0530

    Add files via upload

---
+++ code.ino
@@ -0,0 +1,143 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social networks:            http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ *
+ * This example shows how to use SIM800 or SIM900 modem series
+ * to connect your project to Blynk.
+ *
+ * Attention! Please check out TinyGSM guide:
+ *   http://tiny.cc/tiny-gsm-readme
+ *
+ * WARNING: SIM module support is for BETA testing.
+ *
+ * Change GPRS apm, user, pass, and Blynk auth token to run :)
+ * Feel free to apply it to any other example. It's simple!
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+
+// Default heartbeat interval for GSM is 60
+// If you want override this value, uncomment and set this option:
+//#define BLYNK_HEARTBEAT 30
+#define TINY_GSM_MODEM_SIM800
+#include <TinyGsmClient.h>
+#include <BlynkSimpleSIM800.h>
+#include "DHT.h"
+#define DHTTYPE DHT11 
+
+const int DHTPin = 8;
+DHT dht(DHTPin, DHTTYPE);
+
+// Temporary variables
+static char celsiusTemp[7];
+static char fahrenheitTemp[7];
+static char humidityTemp[7];
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "paste here your auth key";
+
+// Your GPRS credentials
+// Leave empty, if missing user or pass
+char apn[]  = "internet"; //APN of your service provider
+char user[] = "";
+char pass[] = "";
+
+// Hardware Serial on Mega, Leonardo, Micro
+//#define SerialAT Serial1
+
+// or Software Serial on Uno, Nano
+#include <SoftwareSerial.h>
+SoftwareSerial SerialAT(2, 3); // RX, TX
+
+TinyGsm modem(SerialAT);
+
+void setup()
+{
+  // Set console baud rate
+  Serial.begin(19200);
+  delay(10);
+
+  // Set GSM module baud rate
+  SerialAT.begin(19200);
+  delay(3000);
+
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  modem.restart();
+
+  // Unlock your SIM card with a PIN
+  //modem.simUnlock("1234");
+
+  Blynk.begin(auth, modem, apn, user, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+  float h = dht.readHumidity();
+            // Read temperature as Celsius (the default)
+            float t = dht.readTemperature();
+            // Read temperature as Fahrenheit (isFahrenheit = true)
+            float f = dht.readTemperature(true);
+            // Check if any reads failed and exit early (to try again).
+            if (isnan(h) || isnan(t) || isnan(f)) {
+              Serial.println("Failed to read from DHT sensor!");
+              strcpy(celsiusTemp,"Failed");
+              strcpy(fahrenheitTemp, "Failed");
+              strcpy(humidityTemp, "Failed");         
+            }
+            else{
+              // Computes temperature values in Celsius + Fahrenheit and Humidity
+              float hic = dht.computeHeatIndex(t, h, false);       
+              dtostrf(hic, 6, 2, celsiusTemp);             
+              float hif = dht.computeHeatIndex(f, h);
+              dtostrf(hif, 6, 2, fahrenheitTemp);         
+              dtostrf(h, 6, 2, humidityTemp);
+              // You can delete the following Serial.print's, it's just for debugging purposes
+              Serial.print("Humidity: ");
+              Serial.print(h);
+              Serial.print(" %\t Temperature: ");
+              Serial.print(t);
+              Serial.print(" *C ");
+              Serial.print(f);
+              Serial.print(" *F\t Heat index: ");
+              Serial.print(hic);
+              Serial.print(" *C ");
+              Serial.print(hif);
+              Serial.print(" *F");
+              Serial.print("Humidity: ");
+              Serial.print(h);
+              Serial.print(" %\t Temperature: ");
+              Serial.print(t);
+              Serial.print(" *C ");
+              Serial.print(f);
+              Serial.print(" *F\t Heat index: ");
+              Serial.print(hic);
+              Serial.print(" *C ");
+              Serial.print(hif);
+              Serial.println(" *F");
+                  Blynk.virtualWrite(V1,t);
+                    Blynk.virtualWrite(V2,h);
+                       if(t>30) //set temperatur level for notification
+                       {
+                        Blynk.email("email@email.com", "ESP8266 Alert", "Temperature Increased over limit");
+                        Blynk.notify("Temperature Increased over limit");
+            }
+        
+            }
+        
+}
